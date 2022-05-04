53d6adb49268869f734c783aa4e90456c95edd4b
Paulis2 <mariaalhucema@colegioguanenta.edu.co>
2022-05-04 18:10:56 -0500

    Desarrollo: switchejercicios

---
+++ codigo2_js.js
@@ -0,0 +1,30 @@
+function diaseman()
+{
+    dia = parseInt(document.formulario.respuesta.value = "Lunes");
+    switch(día)
+    {
+    case 1: 
+        document.fotmulario.respuesta.value = "Lunes";
+        break;
+    case 2: 
+        documente.formulario.respuesta.value= "Martes";
+        break;  
+    case 3: 
+        document.fotmulario.respuesta.value = "Miercoles";
+        break;
+    case 4: 
+        document.fotmulario.respuesta.value = "Jueves";
+        break;
+    case 5: 
+        document.fotmulario.respuesta.value = "Viernes";
+        break;
+    case 6: 
+        document.fotmulario.respuesta.value = "Sabado";
+        break;
+    case 7: 
+        document.fotmulario.respuesta.value = "Domingo";
+        break;
+    defaull: 
+        document.formulario.respuesta.value = "No es un día de la semana"; 
+    }
+}   
